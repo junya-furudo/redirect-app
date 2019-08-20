b31b4c8e8d5f992d0eaea6416f27ac810661e758
junya-furudo <junya_furudo@dwango.co.jp>
2019-08-20 16:46:12 +0900

    add js file

---
+++ docs/index.js
@@ -0,0 +1,19 @@
+var launch = function () {
+  console.log('called launch!');
+  console.dir(arguments)
+  var e = document.getElementById('jump');
+  var ev = document.createEvent('MouseEvents');
+  ev.initEvent('click',true,true,document.defaultView,1,0,0,0,0,false,false,false,false,0,null);
+  document.body.style.backgroundColor = '#FFFFFF';
+  e.dispatchEvent(ev);
+}
+if (window.navigator.standalone) {
+  if (document.hidden !== undefined) {
+    document.addEventListener('visibilitychange', launch, false);
+  } else if (document.webkitHidden !== undefined) {
+    document.addEventListener('webkitvisibilitychange', launch, false);
+  }
+  launch();
+} else {
+  document.getElementById('description').style.display = '';
+}
